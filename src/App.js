d1704744b2e57d0c9db75e97143fd6fbd07564fe
Negin <negin.khademi@gmail.com>
2024-04-20 13:16:25 +0330

    conditional templates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [showEvevnts, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "negin's brthday bash", id: 1 },
     { title: "nasim's brthday bash", id: 2 },
     { title: "narsis's brthday bash", id: 3 },
   ]);
+  console.log(showEvevnts);
 
   const handleClick = (id) => {
     setEvents((prevState) => {
@@ -20,14 +22,25 @@ function App() {
   };
   return (
     <div className="App">
-      {events.map((event) => (
-        <div key={event.id}>
-          <h1>
-            {event.id}-{event.title}
-          </h1>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
+      {!showEvevnts && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
-      ))}
+      )}
+      {showEvevnts && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {showEvevnts &&
+        events.map((event) => (
+          <div key={event.id}>
+            <h1>
+              {event.id}-{event.title}
+            </h1>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
